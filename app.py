97ccfa8215294ee74678b13fe783d3c53dc95b3f
Keith Ho <keith.ho@goldbeam.local>
2017-10-25 12:10:46 +0800

    First commit, added REST API to access items, stores and user authentication.

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+# security.py
+from security import authenticate, identity
+# user.py
+from user import UserRegister
+#item.py
+from item import Item, Items
+
+
+app = Flask(__name__)
+app.secret_key = 'jose'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+		
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+	app.run(port=80, debug=True)
